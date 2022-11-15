a73c630fc0ea92524c5906110320d4f4807115f3
Team Bifrost <harelehg@gmail.com>
2022-11-15 17:21:50 -0600

    Inicializando el proyecto

---
+++ SceneManagerData.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerData : MonoBehaviour
+{
+    //Cambio de escenas
+    public void LoadScene(string scenename)
+    {
+        SceneManager.LoadScene(scenename);
+    }
+}

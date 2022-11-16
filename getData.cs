63329f9d70d267c5df54aac8bcfd8e49191798af
Team Bifrost <harelehg@gmail.com>
2022-11-15 23:52:53 -0600

    Datos de usuario

---
+++ getData.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class getData : MonoBehaviour
+{
+    public Text nombre, apellido, municipio, comunidad;
+    public Button btnDelete;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nombre.text = PlayerPrefs.GetString("nombre");
+        apellido.text = PlayerPrefs.GetString("apellido");
+        municipio.text = PlayerPrefs.GetString("municipio");
+        comunidad.text = PlayerPrefs.GetString("comunidad");
+    }
+
+    // Update is called once per frame
+    public void eraserPlayerPrefs()
+    {
+        PlayerPrefs.DeleteAll();
+        SceneManager.LoadScene("Uno_Intro");
+    }
+}

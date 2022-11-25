3cc2157bf365ea070e9eae67015a68d4f464d7b6
Hector Romero <harelehg@gmail.com>
2022-11-25 15:07:48 -0600

    Se agregaron las constantes que se usaran en la aplicacion como los colores y descripciones

---
+++ constants.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  //Primary color
+  static var primaryColor = const Color(0xff296e48);
+  static var blackColor = Colors.black54;
+
+  //Onboarding texts
+  static var titleOne = "Fitoterapia";
+  static var descriptionOne =
+      "El uso de los productos vegetales con una finalidad terapéutica.";
+  static var titleTwo = "Utilidad";
+  static var descriptionTwo =
+      "Alivia los sintomas, previene enfermedades, o restablece el estado de salud.";
+  static var titleThree = "Nuestros productos";
+  static var descriptionThree =
+      "En base a la Fitoterapia elaboramos té con plantas medicinales sin nungún conservante artificial.";
+}

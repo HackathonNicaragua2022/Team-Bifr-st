6cd9413f24c39312aa32a122d86704809fb9b43f
Hector Romero <harelehg@gmail.com>
2022-11-25 18:22:20 -0600

    Actualizacion del constant

---
+++ constants.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  //Primary color
+  static var primaryColor = const Color(0xff296e48);
+  static var blackColor = Colors.black54;
+
+  //Onboarding texts
+  static var titleOne = "Bienvenidos";
+  static var descriptionOne =
+      "¿Sabias que en Nicaragua existen zonas vulnerables?.";
+  static var titleTwo = "Alertas";
+  static var descriptionTwo =
+      "Nuestra aplicación cuenta con alerta temprana ante un inminente inundación.";
+  static var titleThree = "Mi Zona Segura";
+  static var descriptionThree =
+      "Mediante nuestra aplicación podras encontrar las zonas seguras proximas a tu ubicación.";
+}

81a631eeb2ece345d98c58a6e7ba009b0c84457f
Hector Romero <harelehg@gmail.com>
2022-11-25 20:41:34 -0600

    Pantalla Inicial de la aplicacion

---
+++ home_page.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_onboarding/constants.dart';
+import 'package:flutter_onboarding/models/instituciones.dart';
+import 'package:flutter_onboarding/ui/screens/detail_page.dart';
+import 'package:flutter_onboarding/ui/screens/widgets/plant_widget.dart';
+import 'package:page_transition/page_transition.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    int selectedIndex = 0;
+    Size size = MediaQuery.of(context).size;
+
+    List<Plant> _plantList = Plant.plantList;
+
+    //Plants category
+    List<String> numeros = [
+      'Bienvenidos',
+    ];
+
+    //Toggle Favorite button
+    bool toggleIsFavorated(bool isFavorited) {
+      return !isFavorited;
+    }
+
+    return Scaffold(
+        body: SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: const EdgeInsets.only(top: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 16.0,
+                  ),
+                  width: size.width * .9,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Constants.primaryColor.withOpacity(.1),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 12),
+            height: 50.0,
+            width: size.width,
+            child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: numeros.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          selectedIndex = index;
+                        });
+                      },
+                      child: Text(
+                        numeros[index],
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: selectedIndex == index
+                              ? FontWeight.bold
+                              : FontWeight.w300,
+                          color: selectedIndex == index
+                              ? Constants.primaryColor
+                              : Constants.blackColor,
+                        ),
+                      ),
+                    ),
+                  );
+                }),
+          ),
+          Container(
+            padding: const EdgeInsets.only(left: 16, bottom: 20, top: 20),
+            child: const Text(
+              'Numeros de Emergencia',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18.0,
+              ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 12),
+            height: size.height * .5,
+            child: ListView.builder(
+                itemCount: _plantList.length,
+                scrollDirection: Axis.vertical,
+                physics: const BouncingScrollPhysics(),
+                itemBuilder: (BuildContext context, int index) {
+                  return GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            PageTransition(
+                                child: DetailPage(
+                                    plantId: _plantList[index].plantId),
+                                type: PageTransitionType.bottomToTop));
+                      },
+                      child: PlantWidget(index: index, plantList: _plantList));
+                }),
+          ),
+        ],
+      ),
+    ));
+  }
+}
